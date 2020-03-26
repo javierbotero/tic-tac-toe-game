9a511523658b0bcc01563685626ff59216aaf044
Javier Botero <javierbotero10271@gmail.com>
2020-03-26 15:43:16 -0500

    Add logic to the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,32 @@
 #!/usr/bin/env ruby
+require './lib/logic'
 
-def user_input(player)
-  user_choice = false
-
+def user_input(player, game)
   puts "please #{player} choose the square that you want to put your mark in:"
   loop do
     user_choice = gets.chomp.to_i
     if user_choice < 10 && user_choice.positive?
-      # treat player choice
-      puts ''
-      break
+      begin
+        game.add_marker(player, user_choice)
+        break
+      rescue ArgumentError => e
+        puts e.message
+      end
     else
       puts 'You should select a number between 1 and 9'
     end
   end
+end
 
-  user_choice
+def display_board(game)
+  puts '-------- Board --------'
+  
+  print '|'
+  game.board.each_with_index do |value, index|
+    print "\n-------------\n|" if [3,6].include?(index)
+    print " #{value.empty? ? ' ' : value} |"
+  end
+  puts ""
 end
 
 def play_again
@@ -24,32 +35,34 @@ def play_again
   loop do
     puts 'do you want to play again?(Y/N)'
     answer = gets.chomp
-    arr = %w[Y y N n]
-    break if arr.include?(answer)
 
-    puts 'Wrong input you must choose Y or N'
+    if answer == 'Y' || answer == 'N' || answer == 'y' || answer == 'n'
+      break
+    else
+      puts 'Wrong input you must choose Y or N'
+    end
   end
 
   answer
 end
 
-def game(player1, player2)
+def no_winner?(game)
   9.times do |number|
     if number.even?
-      user_input(player1)
-
-      # show the board
-      puts '------------ Board ----------'
+      user_input(game.player_x, game)
     else
-      user_input(player2)
-
-      # show the board
-      puts '---------- Board -----------'
+      user_input(game.player_o, game)
     end
 
-    # check for the winner if so print it
-    # puts "#{player1} is the winner" if false
+    display_board(game)
+
+    if game.winner
+      puts "#{ game.winner == "X" ? game.player_x : game.player_o} is the winner"
+
+      return false
+    end
   end
+
   true
 end
 
@@ -63,6 +76,8 @@ def init
     puts 'please player number 2 (O) enter your name:'
     player2 = gets.chomp
 
+    game = TicTacToe.new(player1, player2)
+
     puts '----- this is how you can select your square ------'
     puts 'basically just by choosing the square number'
 
@@ -71,13 +86,9 @@ def init
     puts '-------- Board --------'
     puts "|   |   |   |\n-------------\n|   |   |   |\n-------------\n|   |   |   |"
 
-    no_winner = game(player1, player2)
+    puts 'no winner for now foolks' if no_winner?(game)
 
-    puts 'no winner for now foolks' if no_winner
-
-    answer = play_again
-
-    break if %w[N n].include?(answer)
+    break if %w[N n].include?(play_again)
   end
 end
 
